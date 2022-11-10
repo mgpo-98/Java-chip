38426723ac93a523913d41e379293458889d63fc
riumr <atrium_grate.0j@icloud.com>
2022-11-10 09:48:19 +0900

    ADD : Review모델 작성 [김태형]

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# 이미지
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+# Datetime
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    item_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 95},
+    )
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_reviews"
+    )
