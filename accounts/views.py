72830c75ead387840d9c837772f71790bef3e885
ericaforcoding <ericaforcoding@gmail.com>
2022-11-10 17:15:49 +0900

    Add Accounts index,login,signup,update [오진수]

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from .models import User, Profile
+from django.shortcuts import render, redirect
+from accounts.forms import SignupForm, UpdateForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def index(request):
+    users = User.objects.all()
+    context = {
+        "users": users,
+    }
+    return render(request, "accounts/index.html", context)
+
+def signup(request):
+    if request.method =="POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("accounts:index")
+    else:
+        form = SignupForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get("next") or "accounts:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:index")
+
+@login_required
+def update(request):
+    if request.method == "POST":
+        form = UpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = UpdateForm(instance=request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/update.html", context)
