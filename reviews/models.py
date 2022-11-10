26d198fc610673c659a21b4349b866c2d63bf2e6
riumr <atrium_grate.0j@icloud.com>
2022-11-10 10:17:43 +0900

    ADD reviews, users, items 모델작성 [김태형]

--- reviews/models.py
+++ reviews/models.py
@@ -13,11 +13,12 @@ from datetime import datetime, timedelta
 class Review(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     item_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ProcessedImageField(
-        upload_to="images/",
+        upload_to="reviews/images/",
         blank=True,
         processors=[ResizeToFill(1200, 960)],
         format="JPEG",
@@ -26,3 +27,4 @@ class Review(models.Model):
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="like_reviews"
     )
+    comment = models.ForeignKey()
