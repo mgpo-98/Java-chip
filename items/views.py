5f2ff9e064521dca2240cb1efcbd1ddcd1f1c840
riumr <atrium_grate.0j@icloud.com>
2022-11-14 17:40:16 +0900

    UPDATE : views 크롤링 코드 다른 파일로 이동 [김태형]

--- items/views.py
+++ items/views.py
@@ -1,54 +1,18 @@
 from django.shortcuts import render
-import requests
-from bs4 import BeautifulSoup
+from .crawling import get_items_info, get_item_detail
+from .models import Item
 
-# 상품정보를 크롤링해서 model에 저장. 상품 정보를 모두 보여준다.
-
-
-# 목록페이지 정보 크롤링
-def get_items_info(page, item_nums):
-    items_title_ls = []
-    items_image_url_ls = []
-    items_price_ls = []
-    url = f"https://www.beanbrothers.co.kr/goods/goods_list.php?page={page}&cateCd=001"
-    response = requests.get(url)
-    if response.status_code == 200:
-        html = response.text
-        soup = BeautifulSoup(html, "html.parser")
-        items = soup.select_one("div.item_basket_type")
-        for j in range(1, item_nums + 1):
-            items_title = items.select(
-                f"ul > li:nth-child({j}) > div > div.item_info_cont > div > div.item_tit_box > a > strong"
-            )
-            items_image = items.select(
-                f" ul > li:nth-child({j}) > div > div.item_photo_box > a > img"
-            )
-            items_price = items.select(
-                "ul > li:nth-child(1) > div > div.item_info_cont > div > div.item_money_box > strong > span"
-            )
-            items_title_ls.append(items_title[0].get_text())  # 상품명
-            items_image_url_ls.append(items_image[0]["src"])  # 상품이미지
-            itmes_price_ls.append(items_price[0].get_text())  # 상품 가격
-        return {
-            "titles": items_title_ls,
-            "image_urls": items_image_url_ls,
-            "prices": items_price_ls,
-        }
-    else:
-        print(response.status_code)
-
-
-# 상세페이지 정보 크롤링
-
-# url ='https://www.beanbrothers.co.kr/goods/goods_view.php?goodsNo=1000000216'
-# response = requests.get(url)
-# if response.status_code == 200:
-#     html = response.text
-#     soup = BeautifulSoup(html, "html.parser")
+# 상품정보를 크롤링해서 model에 저장.
 
 
 def index(request):
-    return render(request, "items/index.html")
+    items = Item.objects.all()
+    items_info = get_items_info(1, 9)
+    items.image_url = items_info["image_urls"][0]
+    context = {
+        "image_url": items.image_url,
+    }
+    return render(request, "items/index.html", context)
 
 
 def detail(request, item_pk):
