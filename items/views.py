447a540974fa9f9b3e104f18de0164b677ee85ac
riumr <atrium_grate.0j@icloud.com>
2022-11-17 17:43:07 +0900

    UPDATE : 장바구니 기능 구현 [김태형]

--- items/views.py
+++ items/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .crawling import get_items_info, get_item_detail
-from .models import Item, ItemDetail, FactSheet, AboutThisCoffee
+from .models import Item, ItemDetail, FactSheet, AboutThisCoffee, ItemPicked
 from django.core.paginator import Paginator
+from django.db.models import Count
 
 # 상품정보를 크롤링해서 model에 저장.
 
@@ -53,37 +54,46 @@ def index(request):
 
 def detail(request, item_pk):
     item_detail_object = ItemDetail.objects.all()
-    return render(request, "items/detail.html")
+    context = {
+        "item_pk": item_pk,
+    }
+    return render(request, "items/detail.html", context)
 
 
-# 찜하기(장바구니) : login_required 아님
-def pick(request, item_pk):
+# 담은 상품의 정보를 ItemPicked 테이블에 저장한 후 redirect한다.
+def picking(request):
+    return redirect("items:pick")
+
+
+# 장바구니
+def pick(request):
     # 등록되지 않은 유저의 경우
     # 찜한 아이템이 없는 경우
     # 아이템을 찜한 경우
-    return render(request, "items/pick.html")
+    # picked_items를 itemPicked 테이블에 추가한다
     if request.method == "POST":
-        picked_item = Item.objects.get(pk=item_pk)
-        context = {
-            "picked_item": picked_item,
-        }
-        return redirect("item/pick.html")
-
-
-def test(request):
-    item = Item.objects.all()
-    if len(item) == 0:
-        item_page1 = get_items_info(1, 9)
-        for i in range(9):
-            page1 = Item.objects.create(
-                title=item_page1["titles"][i],
-                image_url=item_page1["image_urls"][i],
-                price=item_page1["prices"][i],
+        item_info = request.POST.get("item_picked")
+        print(item_info)
+        picked_items = Item.objects.filter(pk=item_info)
+        for i in picked_items:
+            picked_item_list = ItemPicked(
+                picked_item=i,
             )
+        picked_item_list.save()
+        picked = ItemPicked.objects.all()
+        picked_count = picked.aggregate(count=(Count("id")))
+    else:
+        picked = ItemPicked.objects.all()
+        picked_count = picked.aggregate(count=(Count("id")))
     context = {
-        "item": item,
+        "picked": picked,
+        "picked_count": picked_count["count"],
     }
-    return render(request, "items/test.html", context)
+    return render(request, "items/pick.html", context)
+
+
+def delete_picked(request):
+    return redirect("items:pick")
 
 
 # 조회수 순 정렬 버튼
