29c52457f1769c9a2ccd535f82337012159504d9
riumr <atrium_grate.0j@icloud.com>
2022-11-21 09:52:10 +0900

    UPDATE : 크롤링코드 수정 [김태형]

--- items/crawling.py
+++ items/crawling.py
@@ -21,7 +21,7 @@ def get_items_info(page, item_nums):
                 f" ul > li:nth-child({j}) > div > div.item_photo_box > a > img"
             )
             items_price = items.select(
-                "ul > li:nth-child(1) > div > div.item_info_cont > div > div.item_money_box > strong > span"
+                f"ul > li:nth-child({j}) > div > div.item_info_cont > div > div.item_money_box > strong > span"
             )
             items_title_ls.append(items_title[0].get_text())  # 상품명
             items_image_url_ls.append(items_image[0]["src"])  # 상품이미지
@@ -39,10 +39,45 @@ def get_items_info(page, item_nums):
 
 
 def get_item_detail(goods_number):
+    item_dict = {
+        1: 1000000216,
+        2: 1000000215,
+        3: 1000000214,
+        4: 1000000194,
+        5: 1000000170,
+        6: 1000000031,
+        7: 1000000025,
+        8: 1000000026,
+        9: 1000000082,
+        10: 1000000068,
+        11: 1000000188,
+        12: 1000000067,
+        13: 1000000124,
+        14: 1000000032,
+        15: 1000000033,
+        16: 1000000111,
+        17: 1000000034,
+        18: 1000000036,
+        19: 1000000037,
+        20: 1000000222,
+        21: 1000000115,
+        22: 1000000114,
+        23: 1000000065,
+        24: 1000000064,
+        25: 1000000202,
+        26: 1000000113,
+        27: 1000000059,
+        28: 1000000058,
+        29: 1000000056,
+        30: 1000000169,
+        31: 1000000125,
+        32: 1000000112,
+        33: 1000000066,
+    }
     main_image_url = []
     main_text_h1 = []
     main_text_p = []
-    url = f"https://www.beanbrothers.co.kr/goods/goods_view.php?goodsNo={goods_number}"
+    url = f"https://www.beanbrothers.co.kr/goods/goods_view.php?goodsNo={item_dict[goods_number]}"
     response = requests.get(url)
     if response.status_code == 200:
         html = response.text
